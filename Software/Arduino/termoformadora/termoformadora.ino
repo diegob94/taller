4006a58904c5b5eff3ff77f9803b520230c09cae
diegob94 <dbim.bena@gmail.com>
2014-06-10 20:15:39 -0400

    carcasa lcd, sketch arduino 2.0
    implementado como maquina de estado (no se bloquea)

---
+++ Software/Arduino/termoformadora/termoformadora.ino
@@ -0,0 +1,83 @@
+#include "Wire.h"
+#include "LiquidCrystal.h"
+#include <ClickEncoder.h>
+#include <TimerOne.h>
+//LiquidCrystal lcd(10, 9, 5, 6, 7, 8);
+
+LiquidCrystal lcd(10, 9, 5, 6, 7, 8);
+ClickEncoder *encoder;
+int16_t value,pas;
+
+void timerIsr() {
+  encoder->service();
+}
+
+void displayAccelerationStatus() {
+  lcd.setCursor(0, 1);  
+  lcd.print("Acceleration ");
+  lcd.print(encoder->getAccelerationEnabled() ? "on " : "off");
+}
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+  encoder = new ClickEncoder(A1, A2, A0);
+  encoder->setAccelerationEnabled(false);
+  Timer1.initialize(100);
+  Timer1.attachInterrupt(timerIsr); 
+}
+
+float tiempo=0;
+enum enumestado { 
+  STAP, WARM };
+enumestado estado=STAP;
+unsigned long t0=0;
+int dt=0;
+void loop() {
+  switch(estado){
+  case STAP:
+    {
+      value += encoder->getValue();
+      if(value!=pas){
+        pas=value;
+        if(value > 0){
+          tiempo=float(value/4)/1.0;
+        }
+        else {
+          value=0;
+          tiempo=0;
+        }
+      }
+      ClickEncoder::Button b = encoder->getButton();
+      if (b != ClickEncoder::Open) {
+        if(b==ClickEncoder::Clicked){
+          digitalWrite(13,HIGH);
+          t0=millis();
+          dt=int(tiempo)*1000;
+          value=0;
+          estado=WARM;
+        }
+      }
+      lcd.setCursor(0,0);
+      lcd.print("Tiempo");
+      lcd.setCursor(0,1);
+      lcd.print(tiempo);
+      break;
+    }
+  case WARM:
+    {
+      if(millis()-t0<dt){
+        lcd.setCursor(0,0);
+        lcd.print("Tiempo");
+        lcd.setCursor(0,1);
+        lcd.print(float(dt-millis()+t0)/1000.0);
+      } else {
+        digitalWrite(13,LOW);
+        dt=0;
+        estado=STAP;
+      }
+      break;
+    }
+  }
+}
